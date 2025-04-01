b1e817bd002b1f6942b9da4c1e9d9584c2a12dcd
g1ll <gillvelleda@gmail.com>
2025-04-01 11:26:36 -0300

    feat: alteraco exemplo 04 aula 02

--- topico_02/exemplos/04_reactjs_primeiro_componente/src/App.jsx
+++ topico_02/exemplos/04_reactjs_primeiro_componente/src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css';
-import CardImc from './components/CardImc/CardImc.jsx';
+import CardImc from './components/CardImc/CardImc';
+
+const pessoas = [
+  { name: 'Fulano', altura: 1.7, peso: 90 },
+  { name: 'Beltrano', altura: 1.8, peso: 75 },
+  { name: 'Cicrano', altura: 1.9, peso: 90 },
+  { name: 'Zé', altura: 1.6, peso: 60 },
+  { name: 'Maria', altura: 1.5, peso: 50 },
+  { name: 'João', altura: 1.4, peso: 40 },
+  { name: 'Ana', altura: 1.3, peso: 30 },
+  { name: 'Pedro', altura: 1.2, peso: 20 },
+  { name: 'Paulo', altura: 1.1, peso: 10 },
+  { name: 'Lucas', altura: 1.0, peso: 5 },
+  { name: 'Thiago', altura: 1.9, peso: 95 },
+  { name: 'Fernanda', altura: 1.8, peso: 80 },
+  { name: 'Juliana', altura: 1.7, peso: 70 },
+  { name: 'Roberto', altura: 1.6, peso: 60 },
+  { name: 'Mariana', altura: 1.5, peso: 50 },
+  { name: 'Ricardo', altura: 1.4, peso: 40 },
+];
+
 
 function App() {
   return (
     <>
-      <CardImc key="p1" pessoa={{ name: 'Fulano', altura: 1.7, peso: 90 }} />
-      <CardImc key="p2" pessoa={{ name: 'Beltrano', altura: 1.8, peso: 75 }} />
-      <CardImc key="p3" pessoa={{ name: 'Cicrano', altura: 1.9, peso: 90 }} />
+      {pessoas.map((pessoa, index) => (
+           <CardImc key={index} pessoa={pessoa}/>
+      ))}
     </>
   );
 }
--- topico_02/exemplos/04_reactjs_primeiro_componente/src/components/CardImc/CardImc.jsx
+++ topico_02/exemplos/04_reactjs_primeiro_componente/src/components/CardImc/CardImc.jsx
@@ -8,11 +8,16 @@ export default function CardImc(props) {
   const calcImc = () => (peso / alt ** 2).toFixed(2);
 
   return (
-    <div className="imcCard">
+    <div className="imcCard" style={{
+      backgroundColor: 
+        calcImc() < 25 
+          ? '#00FF00' 
+          : '#FF0000',
+    }}>
       <h1>{props.pessoa.name}:</h1>
       <p>Altura: {alt} m</p>
       <p>Peso: {peso}</p>
-      <p>Imc: {calcImc}</p>
+      <p>Imc: {calcImc()}</p>
     </div>
   );
 }
