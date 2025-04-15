2d2b1215f9a8ba44672118b8be53d04974ed51d3
g1ll <gillvelleda@gmail.com>
2025-04-15 17:25:17 -0300

    fix: adicionando exceções do eslint para não ficar acusando erro no código.

--- topico_03/exemplos/09_site_content_filter/src/components/SearchBar/SearchBar.jsx
+++ topico_03/exemplos/09_site_content_filter/src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useEffect, useState, useRef  } from "react"
 import "./searchBar.css"
