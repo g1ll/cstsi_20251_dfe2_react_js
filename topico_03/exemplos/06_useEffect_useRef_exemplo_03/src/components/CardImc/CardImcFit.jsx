45776f18e8dfa979b1ee0c7e2d2f7b7142b35295
g1ll <gillvelleda@gmail.com>
2025-04-15 11:16:00 -0300

    refactor: comentando exemplos de state e effect do card imc

--- topico_03/exemplos/06_useEffect_useRef_exemplo_03/src/components/CardImc/CardImcFit.jsx
+++ topico_03/exemplos/06_useEffect_useRef_exemplo_03/src/components/CardImc/CardImcFit.jsx
@@ -1,16 +1,32 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState,useRef } from 'react';
 import './cards.css';
 
+const calcImc = (peso, altura) =>peso / altura ** 2;
+
 export default function CardImc({ pessoa }) {
-  const alt = pessoa.altura;
-  const [peso, setPeso] = useState(pessoa.peso);
-  const [imc, setImc] = useState((peso / alt ** 2).toFixed(2));
+  const alt = pessoa.altura; //Não será alterado durante as renderizações
+  
+  //estados podem sofrer alterações durante as renderizações
+  //useState retorna um array com o valor do estado e a função para atualizá-lo
+  const [peso, setPeso] = useState(pessoa.peso); 
+  const [imc, setImc] = useState(calcImc(peso, alt).toFixed(2));
   const [isAtleta, setIsAtleta] = useState(false)
+  
+  //useRef retorna um objeto mutável que persiste durante o ciclo de vida do componente
+  //ele não causa re-renderização quando seu valor é alterado
+  //ele é usado para acessar elementos DOM diretamente
+  //ou para armazenar valores que não precisam causar re-renderização
+  //exemplo: inputEsportesRef.current.checked
   const inputEsportesRef = useRef(null)
   
-
   useEffect(() => {
-    setTimeout(() => setPeso(peso + (!isAtleta?1:-1)), 2000);
+    let timer = setTimeout(() => setPeso(peso=>{
+      let novoPeso = peso + (!isAtleta?1:-1)
+      setImc(calcImc(novoPeso, alt).toFixed(2))
+      return novoPeso;
+    }), 3000);
+    return ()=>clearTimeout(timer) //limpa o timer quando o componente é desmontado ou quando o efeito é re-executado
   }, [peso]);
 
   useEffect(()=>{
@@ -28,10 +44,10 @@ export default function CardImc({ pessoa }) {
       <p>Altura: {alt} m</p>
       <p>Peso: {peso}</p>
       <p>Imc: {imc}</p>
-      <p>{isAtleta?'Atleta':<b>Sedentário</b>}</p>
+      <p>{isAtleta?<i>Atleta</i>:<b>Sedentário</b>}</p>
       <p>
         <input 
-          type='radio' 
+          type='checkbox' 
           ref={inputEsportesRef}
           onChange={atualizaRotina}
         />
