6df188b71dcb4048ca0db995ba8af0be2f4fd229
g1ll <gillvelleda@gmail.com>
2025-04-22 00:01:59 -0300

    feat: exemplos aula 4 react-router-dom

---
+++ topico_04/exercicios/03-routes-neted-outlet-front/src/config/routes.jsx
@@ -0,0 +1,25 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from "react-router-dom";
+import Guest from "../layouts/Guest";
+import Home from "../pages/Home/Home";
+import Show from "../pages/Show/Show";
+import Login from "../pages/Login/Login";
+import NotFound from "../pages/NotFound/NotFound";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<Guest />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/produto/:id" element={<Show />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />}></Route>
+            </Route>
+        </>
+    )
+);
+
+export default router;
