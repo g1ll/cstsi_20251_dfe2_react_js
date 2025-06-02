56ace858a875de3e17ae155cac545bd83d92a138
g1ll <gillvelleda@gmail.com>
2025-06-02 16:57:34 -0300

    fix: ajuste de cores do header

--- topico_06/13_tailwind_front_completo/src/components/Header.jsx
+++ topico_06/13_tailwind_front_completo/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="pb-10 bg-green-600 h-fit">
+    <header className="pb-10 bg-green-300 h-fit">
       <div className="flex flex-col items-center justify-center">
         <h1 className="w-full mt-12 mb-6 text-6xl font-bold text-center text-blue-800 shadow-white">
           Black Friday BuyTech
