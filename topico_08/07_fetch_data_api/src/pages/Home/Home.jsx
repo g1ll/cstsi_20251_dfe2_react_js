7f81d84a9e56d7623635de414cd680467ac55627
g1ll <gillvelleda@gmail.com>
2025-06-17 12:08:52 -0300

    refactor: exemplo função async/await no exemplo 07

--- topico_08/07_fetch_data_api/src/pages/Home/Home.jsx
+++ topico_08/07_fetch_data_api/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import "./home.css"
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import { Cards } from "../../components/Cards/Cards";
 // import { mockedProducts } from "../../data/mockedProducts";
 
@@ -8,35 +8,56 @@ import { Cards } from "../../components/Cards/Cards";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const Home = () => {
-  const [listProdutos,setListProdutos] = useState([])
+  const [listProdutos, setListProdutos] = useState([])
 
   useEffect(() => {
+
+    console.log(API_URL);
     // setTimeout(()=>{      
     //   setListProdutos(mockedProducts)
     // },1000);
 
     //Promise Chain (then().then().catch())
-    console.log(API_URL);
     // fetch(`${API_URL}/produtos`)
     //   .then(response=>response.json())
     //   .then(json=>{
     //     console.log({json})
     //     setListProdutos(json.data)
     //   })
-    //   .catch(error=>console.log(error));
+    //   .catch(error=>console.log(error))
+    //   .finally(()=>console.log("Exemplo com fetch!!"));
+
+
 
     //Async/Await
-    (async ()=>{//IIFE Funções Invocadas Imediatamente
+    const loadData = async () => {
       try {
         const response = await fetch(`${API_URL}/produtos`);
         const data = await response.json();
         console.log(data)
-        setListProdutos(data);
+        setListProdutos(data.data);
       } catch (error) {
         console.error(error)
-      }   
-    })();
-  }, [])
+      } finally {
+        console.log("Exemplo com async/await!!")
+      }
+    }
+    loadData()
+
+    // (async ()=>{//IIFE Funções Invocadas Imediatamente
+    //   try {
+    //     const response = await fetch(`${API_URL}/produtos`);
+    //     const data = await response.json();
+    //     console.log(data)
+    //     setListProdutos(data.data);
+    //   } catch (error) {
+    //     console.error(error)
+    //   } finally{
+    //       console.log("Exemplo com async/await com IIFE!!")
+    //   }     
+    // })();
+
+  }, []) //Final do useEffect
 
 
   return (
@@ -45,7 +66,7 @@ const Home = () => {
         <div className="products_grid_container">
           {!listProdutos?.length
             ? <p>Carregando...</p>
-            :listProdutos.map((product, key) => (
+            : listProdutos.map((product, key) => (
               <Cards key={`card${key}`} item={product} />
             ))
           }
